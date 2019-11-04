02b4acc27559d8802c947aeb3f75a3a0bd8d44d1
Robert <cj92robert@gmail.com>
2019-11-04 11:11:04 +0100

    Initial commit

---
+++ src/main/java/com/example/lessonjdbc/LessonjdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.lessonjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonjdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonjdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lessonjdbc/LessonjdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lessonjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonjdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
